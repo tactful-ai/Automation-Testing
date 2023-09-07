1ecbb50feffb76a00f93d0e875699adaef66a4ad
tonyRaafat <138820743+tonyRaafat@users.noreply.github.com>
2023-09-07 15:02:19 +0300

    Update moniter.js

--- cypress/page-objects/moniter.js
+++ cypress/page-objects/moniter.js
@@ -1,9 +1,10 @@
-import {type, click, dropDown, waitFor } from "./actions";
+import { type, click, dropDown, waitFor } from "./actions";
+import { conditions } from "./conditions";
 
 class Moniter {
-    url = "https://app.qa.dev.tactful.ai/v/engage/engagement-hub/history"
+  url = "https://app.qa.dev.tactful.ai/v/engage/engagement-hub/history";
 
-    
+
   isContainRecords() {
     return cy
       .get('td [role="alert"]  ')
@@ -11,25 +12,64 @@ class Moniter {
       .should("exist")
       .and("have.length", 1);
   }
-  clickReset(){
-    cy.contains("button", "Reset", { timeout: 1000 }).click({force:true});
-  }
-  drobDownFilter(drobdown, list,text,assertfn=this.isContainRecords,args=[]) {
-
-    cy.goTo(this.url)
-    waitFor(".loader-container", "not.exist");
+  clickReset() {
     
-    dropDown(drobdown,list,text,{clickConfig:{timeout:1000}})
-    assertfn(...args);
+    cy.contains("button", "Reset", { timeout: 1000 }).click({ force: true });
+  }
+  drobDownFilter(
+    drobdown,
+    list,
+    text,
+    assertfn = this.isContainRecords,
+    args = []
+  ) {
+    cy.goTo(this.url);
+    waitFor(".loader-container", "not.exist");
+
+    dropDown(drobdown, list, text);
+    // assertfn(...args);
+    conditions.if('td [role="alert"]',()=>{this.isContainRecords()},assertfn(...args))
   }
 
-  inputFilter(inputSelector,text,assertfn=this.isContainRecords,args=[]){
-    cy.goTo(this.url)
+  drobDownFilterOnly(
+    drobdown,
+    list,
+    text,
+    assertfn = this.isContainRecords,
+    args = []
+  ) {
+    cy.goTo(this.url);
     waitFor(".loader-container", "not.exist");
-    type(inputSelector,text)
-    assertfn(...args);
+
+    this.clickReset();
+    dropDown(drobdown, list, text);
+    conditions.if('body td [role="alert"]',()=>{this.isContainRecords()},()=>{assertfn(...args)})
+  }
+
+  inputFilter(
+    inputSelector,
+    text,
+    assertfn = this.isContainRecords,
+    args = []
+  ) {
+    cy.goTo(this.url);
+    waitFor(".loader-container", "not.exist");
+    type(inputSelector, text);
+    conditions.if('body td [role="alert"]',()=>{this.isContainRecords()},()=>{assertfn(...args)})
+  }
+  inputFilterOnly(
+    inputSelector,
+    text,
+    assertfn = this.isContainRecords,
+    args = []
+  ) {
+    cy.goTo(this.url);
+    waitFor(".loader-container", "not.exist");
+    this.clickReset();
+    type(inputSelector, text);
+    conditions.if('body td [role="alert"]',()=>{this.isContainRecords()},()=>{assertfn(...args)})
   }
 }
 
 const moniter = new Moniter();
-export default moniter
+export default moniter;
